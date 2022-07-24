c0cca5805bdd1fcfc606d06f478d41df87071acd
Lucas Piputto <lucaspiputto@gmail.com>
2022-07-24 15:20:28 -0300

    Render first texture

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,14 @@
 int main(int argc, char* argv[]) 
 {
   if (SDL_Init(SDL_INIT_VIDEO) > 0)
-    std::cout << "HEY.. SDL_Init has failed. SDL_ERROR: " << SDL_GetError() << std::endl;
+    std::cout << "SDL_Init has failed. SDL_ERROR: " << SDL_GetError() << std::endl;
 
   if (!(IMG_Init(IMG_INIT_PNG)))
-    std::cout << "IMG_Init has failed. Error: " << SDL_GetError() << std::endl;
+    std::cout << "IMG_Init has failed. SDL_ERROR: " << SDL_GetError() << std::endl;
 
   RenderWindow window("GAME v1.0", 1280, 720);
+
+  SDL_Texture* gooseTexture = window.loadTexture("res/gfx/goose.png");
 
   bool gameRunning = true;
 
@@ -25,6 +27,10 @@ int main(int argc, char* argv[])
       if (event.type == SDL_QUIT)
         gameRunning = false;
     }
+
+    window.clear();
+    window.render(gooseTexture);
+    window.display();
   }
 
   window.cleanUp();
--- src/renderWindow.cpp
+++ src/renderWindow.cpp
@@ -5,19 +5,45 @@
 #include "RenderWindow.hpp"
 
 RenderWindow::RenderWindow(const char* p_title, int p_w, int p_h)
-  :window(NULL), renderer(NULL)
+  :window(nullptr), renderer(nullptr)
 {
   window = SDL_CreateWindow(p_title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, p_w, p_h, SDL_WINDOW_SHOWN);
 
-  if (window == NULL)
+  if (window == nullptr)
   {
-    std::cout << "Window failed to init. Error: " << SDL_GetError() << std::endl;
+    std::cout << "Window failed to init. SDL_ERROR: " << SDL_GetError() << std::endl;
   }
 
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
 
+SDL_Texture* RenderWindow::loadTexture(const char* p_filePath)
+{
+  SDL_Texture* texture = nullptr;
+  texture = IMG_LoadTexture(renderer, p_filePath);
+
+  if (texture == nullptr)
+    std::cout << "Failed to load texture. SDL_ERROR: " << SDL_GetError() << std::endl;
+
+  return texture;
+}
+
 void RenderWindow::cleanUp()
 {
   SDL_DestroyWindow(window);
+}
+
+void RenderWindow::clear()
+{
+  SDL_RenderClear(renderer);
+}
+
+void RenderWindow::render(SDL_Texture* p_tex)
+{
+  SDL_RenderCopy(renderer, p_tex, nullptr, nullptr);
+}
+
+void RenderWindow::display()
+{
+  SDL_RenderPresent(renderer);
 }
