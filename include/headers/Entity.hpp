9763385aac95a3a85c448b74943515731f9e3785
Lucas Piputto <lucaspiputto@gmail.com>
2022-08-03 17:54:47 -0300

    Add draw method to Entity class

--- include/headers/Entity.hpp
+++ include/headers/Entity.hpp
@@ -7,12 +7,14 @@
 class Entity
 {
 public:
-  Entity(SDL_Texture* p_tex, Vector2i p_pos, Vector2i p_prop);
+  Entity(SDL_Renderer* p_renderer, SDL_Texture* p_tex, Vector2i p_pos, Vector2i p_prop);
+  void draw();
   Vector2i& getPos();
   Vector2i& getProp();
   SDL_Texture* getTex();
   SDL_Rect& getCurrentFrame();
 private:
+  SDL_Renderer* renderer;
   Vector2i pos;
   Vector2i prop;
   SDL_Texture* tex;
--- src/entity.cpp
+++ src/entity.cpp
@@ -5,13 +5,18 @@
 #include "Entity.hpp"
 #include "GameMath.hpp"
 
-Entity::Entity(SDL_Texture* p_tex, Vector2i p_pos, Vector2i p_prop)
-  :pos(p_pos), prop(p_prop), tex(p_tex)
+Entity::Entity(SDL_Renderer* p_renderer, SDL_Texture* p_tex, Vector2i p_pos, Vector2i p_prop)
+  :renderer(p_renderer), pos(p_pos), prop(p_prop), tex(p_tex)
 {
   currentFrame.x = 0;
   currentFrame.y = 0;
   currentFrame.w = prop.x;
   currentFrame.h = prop.y;
+}
+
+void Entity::draw()
+{
+  SDL_RenderCopy(renderer, tex, NULL, NULL);
 }
 
 Vector2i& Entity::getPos()
